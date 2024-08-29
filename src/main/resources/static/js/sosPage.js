3042d2890efa08ee5a537f3fefc5054053a14c5d
GiovanniBottesini <larkda2@nate.com>
2024-08-29 21:42:51 +0900

    1212

--- src/main/resources/static/js/sosPage.js
+++ src/main/resources/static/js/sosPage.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-    // 페이지 로드 시 체크 상태에 따라 함수 실행
-    if ($("#btnradio1").is(":checked")) {
-        $("#sosPage").load("/soshospitallist/content1");
-    } else if ($("#btnradio2").is(":checked")) {
+    // 페이지 로드 시 URL 파라미터에 따라 라디오 버튼 상태를 설정하고 해당 콘텐츠 로드
+    const urlParams = new URLSearchParams(window.location.search);
+    const isRadio2Selected = urlParams.get('isRadio2Selected');
+
+    if (isRadio2Selected === 'true') {
+        $("#btnradio2").prop("checked", true);
         $("#sosPage").load("/soshospitallist/content2");
+    } else {
+        $("#btnradio1").prop("checked", true);
+        $("#sosPage").load("/soshospitallist/content1");
     }
 
-    // 라디오 버튼 클릭 시 해당 컨텐츠 로드
+    // 라디오 버튼 클릭 시 URL을 변경하고 해당 콘텐츠 로드
     $("#btnradio1").on("click", function() {
+        window.history.pushState(null, null, "/soshospitallist");
         $("#sosPage").load("/soshospitallist/content1");
     });
 
     $("#btnradio2").on("click", function() {
+        window.history.pushState(null, null, "/soshospitallist?isRadio2Selected=true");
         $("#sosPage").load("/soshospitallist/content2");
     });
 });
