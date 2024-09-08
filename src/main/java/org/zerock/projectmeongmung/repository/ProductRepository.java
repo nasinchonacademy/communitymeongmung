45b4c119aff1ad715988f9c4f8e2f67551103d31
yunakang <misoyuna@naver.com>
2024-09-09 08:50:15 +0900

    "update shop category"

--- src/main/java/org/zerock/projectmeongmung/repository/ProductRepository.java
+++ src/main/java/org/zerock/projectmeongmung/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package org.zerock.projectmeongmung.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.projectmeongmung.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 // 기본적인 CRUD 메서드는 JpaRepository에서 제공
+    List<Product> findByPcategory(String pcategory);  // 카테고리별 상품 조회
 }
