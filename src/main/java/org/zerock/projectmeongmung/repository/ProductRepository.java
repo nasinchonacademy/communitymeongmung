6fe167d17a60a8d2e03bf614abe8e13b14fd097a
GiovanniBottesini <larkda2@nate.com>
2024-09-09 11:34:40 +0900

    1. 메인 배너 이벤트 추가 2. 배너 및 이벤트에 링크 추가 3. productService 추가 내용
        public List<ProductDTO> getTop3StoriesByPid() {
            // Product 리스트를 가져옵니다.
            List<Product> topProducts = productRepository.findTop3ByOrderByPidDesc();
            // Product 리스트를 ProductDTO 리스트로 변환하여 반환합니다.
            return topProducts.stream()
                    .map(this::entityToDto) // Product를 ProductDTO로 변환
                    .collect(Collectors.toList());
        }
    
            private ProductDTO entityToDto(Product product) {
                return new ProductDTO(
                        product.getPid(),
                        product.getPname(),
                        product.getPprice(),
                        product.getPcategory(),
                        product.getPdescription(),
                        product.getPcompany(),
                        product.getPstock(),
                        product.getProductphoto()
                );
            }
    
    4. ProductRepository 추가 내용
    
        List<Product> findTop3ByOrderByPidDesc();

--- src/main/java/org/zerock/projectmeongmung/repository/ProductRepository.java
+++ src/main/java/org/zerock/projectmeongmung/repository/ProductRepository.java
@@ -1,8 +1,15 @@
 package org.zerock.projectmeongmung.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.projectmeongmung.dto.ProductDTO;
+import org.zerock.projectmeongmung.entity.MeongStory;
 import org.zerock.projectmeongmung.entity.Product;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 // 기본적인 CRUD 메서드는 JpaRepository에서 제공
+
+    List<Product> findTop3ByOrderByPidDesc();
+
 }
