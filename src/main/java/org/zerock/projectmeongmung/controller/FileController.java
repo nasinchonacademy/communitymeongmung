bb114513cce22f5a083ebc6a1293a5c81dec808d
GiovanniBottesini <larkda2@nate.com>
2024-09-09 15:55:29 +0900

    1

--- src/main/java/org/zerock/projectmeongmung/controller/FileController.java
+++ src/main/java/org/zerock/projectmeongmung/controller/FileController.java
@@ -22,7 +22,7 @@ public class FileController {
             // 파일이 저장된 실제 경로
             // /Users/yunakang/uploads/
             // "C:\\work\\uploads\\"
-            Path filePath = Paths.get("/Users/yunakang/uploads/").resolve(filename);
+            Path filePath = Paths.get("C:\\work\\uploads\\").resolve(filename);
             Resource file = new UrlResource(filePath.toUri());
 
             // 파일이 존재하고 읽을 수 있는지 확인
@@ -41,7 +41,7 @@ public class FileController {
     public String saveProfilePhoto(MultipartFile file) throws IOException {
         // 저장할 경로 설정
         // /Users/yunakang/uploads/
-        String uploadDir = "/Users/yunakang/uploads/";
+        String uploadDir = "C:\\work\\uploads\\";
 
         // 파일명을 고유하게 하기 위해 UUID를 사용하거나 다른 로직을 사용할 수 있습니다.
         String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
@@ -58,6 +58,6 @@ public class FileController {
 
     // 파일 경로를 생성하는 메서드
     public String getProfilePhotoPath(String filename) {
-        return Paths.get("/Users/yunakang/uploads/").resolve(filename).toString();
+        return Paths.get("C:\\work\\uploads\\").resolve(filename).toString();
     }
 }
